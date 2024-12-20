1e350b2f4d65fe91e36871c34ec66ff7336bafa1
Nahom32 <senaynahom00@gmail.com>
2024-12-20 05:06:35 +0300

    feat: added dfs

--- question-1.py
+++ question-1.py
@@ -10,7 +10,7 @@ def build_networkxGraph(roads):
 def visualize_graph(graph):
     edge_labels = nx.get_edge_attributes(graph,'weight')
     pos = nx.spring_layout(graph)
-    nx.draw(graph, pos, with_labels=True, node_color='lightblue', node_size=3000, font_size=10, font_weight='bold', edge_color='gray')
+    nx.draw(graph, pos, with_labels=True, node_color='red', node_size=500, font_size=5, font_weight='bold', edge_color='gray')
     nx.draw_networkx_edge_labels(graph, pos, edge_labels=edge_labels, font_color='red', font_size=8)
     plt.title("Travel Ethiopia")
     plt.show()
@@ -28,6 +28,34 @@ if __name__ == '__main__':
     'Hawassa': [('Addis Ababa', 275)],
     'Mekelle': [('Gondar', 300)]
     }
-    main(roads)
+    cities2 = [
+    'Arad', 'Bucharest', 'Craiova', 'Dobreta', 'Eforie', 'Fagaras', 
+    'Giurgiu', 'Hirsova', 'Iasi', 'Lugoj', 'Mehadia', 'Neamt', 
+    'Oradea', 'Pitesti', 'Rimnicu Vilcea', 'Sibiu', 'Timisoara', 'Urziceni', 'Vaslui', 'Zerind'
+    ]
+
+    roads2 = {
+    'Arad': [('Zerind', 75), ('Sibiu', 140), ('Timisoara', 118)],
+    'Zerind': [('Arad', 75), ('Oradea', 71)],
+    'Oradea': [('Zerind', 71), ('Sibiu', 151)],
+    'Sibiu': [('Arad', 140), ('Oradea', 151), ('Fagaras', 99), ('Rimnicu Vilcea', 80)],
+    'Timisoara': [('Arad', 118), ('Lugoj', 111)],
+    'Lugoj': [('Timisoara', 111), ('Mehadia', 70)],
+    'Mehadia': [('Lugoj', 70), ('Dobreta', 75)],
+    'Dobreta': [('Mehadia', 75), ('Craiova', 120)],
+    'Craiova': [('Dobreta', 120), ('Rimnicu Vilcea', 146), ('Pitesti', 138)],
+    'Rimnicu Vilcea': [('Sibiu', 80), ('Craiova', 146), ('Pitesti', 97)],
+    'Fagaras': [('Sibiu', 99), ('Bucharest', 211)],
+    'Pitesti': [('Rimnicu Vilcea', 97), ('Craiova', 138), ('Bucharest', 101)],
+    'Bucharest': [('Fagaras', 211), ('Pitesti', 101), ('Giurgiu', 90), ('Urziceni', 85)],
+    'Giurgiu': [('Bucharest', 90)],
+    'Urziceni': [('Bucharest', 85), ('Hirsova', 98), ('Vaslui', 142)],
+    'Hirsova': [('Urziceni', 98), ('Eforie', 86)],
+    'Eforie': [('Hirsova', 86)],
+    'Vaslui': [('Urziceni', 142), ('Iasi', 92)],
+    'Iasi': [('Vaslui', 92), ('Neamt', 87)],
+    'Neamt': [('Iasi', 87)]
+    }
+    main(roads2)
 
 
---
+++ question-2.py
@@ -0,0 +1,42 @@
+from typing import Dict
+def depth_first_search(graph:Dict,start_node, end_node):
+    '''
+
+    '''
+    if not(start_node in graph and end_node in graph):
+        return []
+    stack = [(start_node,[start_node])]
+    while stack:
+        curr_node,path = stack.pop()
+        if curr_node == end_node:
+            return path
+        for i in graph[curr_node]:
+            if i[0] not in path:
+                stack.append((i[0],path+[i[0]]))
+    return []
+
+roads2 = {
+    'Arad': [('Zerind', 75), ('Sibiu', 140), ('Timisoara', 118)],
+    'Zerind': [('Arad', 75), ('Oradea', 71)],
+    'Oradea': [('Zerind', 71), ('Sibiu', 151)],
+    'Sibiu': [('Arad', 140), ('Oradea', 151), ('Fagaras', 99), ('Rimnicu Vilcea', 80)],
+    'Timisoara': [('Arad', 118), ('Lugoj', 111)],
+    'Lugoj': [('Timisoara', 111), ('Mehadia', 70)],
+    'Mehadia': [('Lugoj', 70), ('Dobreta', 75)],
+    'Dobreta': [('Mehadia', 75), ('Craiova', 120)],
+    'Craiova': [('Dobreta', 120), ('Rimnicu Vilcea', 146), ('Pitesti', 138)],
+    'Rimnicu Vilcea': [('Sibiu', 80), ('Craiova', 146), ('Pitesti', 97)],
+    'Fagaras': [('Sibiu', 99), ('Bucharest', 211)],
+    'Pitesti': [('Rimnicu Vilcea', 97), ('Craiova', 138), ('Bucharest', 101)],
+    'Bucharest': [('Fagaras', 211), ('Pitesti', 101), ('Giurgiu', 90), ('Urziceni', 85)],
+    'Giurgiu': [('Bucharest', 90)],
+    'Urziceni': [('Bucharest', 85), ('Hirsova', 98), ('Vaslui', 142)],
+    'Hirsova': [('Urziceni', 98), ('Eforie', 86)],
+    'Eforie': [('Hirsova', 86)],
+    'Vaslui': [('Urziceni', 142), ('Iasi', 92)],
+    'Iasi': [('Vaslui', 92), ('Neamt', 87)],
+    'Neamt': [('Iasi', 87)]
+    }
+print(depth_first_search(roads2,'Arad','Sibiu'))
+
+
